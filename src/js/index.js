5618855aaf70e27cc0e98f453c363a48dff6c9d5
anthony-pinhooo <contato.anthony27@gmail.com>
2023-04-14 21:54:23 -0400

    pokedex terminada

--- src/js/index.js
+++ src/js/index.js
@@ -30,7 +30,36 @@ const getPokemons = async (id) => {
     const url = `https://pokeapi.co/api/v2/pokemon/${id}`
     const resp = await fetch(url)
     const data = await resp.json()
-    console.log(data);
+    createPokemonCard(data)
+    // console.log(data);
+}
+
+const createPokemonCard = (poke) => {
+    const card = document.createElement('div')
+    card.classList.add("pokemon")
+
+    const name = poke.name[0].toUpperCase() + poke.name.slice(1)
+    const id = poke.id.toString().padStart(3, '0')
+
+    const pokeTypes = poke.types.map(type => type.type.name)
+    const type = mainTypes.find(type => pokeTypes.indexOf(type) > -1)
+    const color = colors[type]
+
+    card.style.backgroundColor = color
+
+    const pokemonInnerHTML = `
+    <div class="imgContainer">
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${poke.id}.png" alt="${name}">  
+    </div>
+    <div class="info">
+        <span class="number">${id}</span>
+        <h3 class="name">${name}</h3>
+        <small class="type">Type: <span>${type}</span></small>
+    </div>
+    `
+    card.innerHTML = pokemonInnerHTML
+
+    pokeContainer.appendChild(card)
 }
 
 fetchPokemons()
